b875a613f9bc79f34d26bc3eba0a6b579e305f63
robbairbadeia <robber.badeia@gmail.com>
2023-07-13 03:40:02 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,12 +11,13 @@ int main(void)
 	float f;
 	double d;
 
-	printf("Size of a char: %uz byte(s)", sizeof(c));
-	printf("Size of an int: %uz byte(s)", sizeof(i));
-	printf("Size of a long: %uz byte(s)", sizeof(f));
+	printf("Size of a char: %uz byte(s)\n", sizeof(c));
+	printf("Size of an int: %uz byte(s)\n", sizeof(i));
+	printf("Size of a long: %uz byte(s)\n", sizeof(f));
+	printf("Size of a char: %uz byte(s)\n", sizeof(char));
+	printf("Size of a char: %uz byte(s)\n", sizeof(char));
 
-	printf("Size of a char: %uz byte(s)", sizeof(char));
-	printf("Size of a char: %uz byte(s)", sizeof(char));
 
-	return (0);
+
+
 }
