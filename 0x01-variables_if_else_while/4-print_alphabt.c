4ba3b4eba09af9a77f8a7ba924364cc0108cfee1
robbairbadeia <robber.badeia@gmail.com>
2023-07-14 04:35:38 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if (alpha != 'q' || alpha != 'e')
+		{
+			putchar(alpha);
+		}
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
