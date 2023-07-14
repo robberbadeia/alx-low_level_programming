522a456cacad5a715750787722e7de5ccfc5090b
robbairbadeia <robber.badeia@gmail.com>
2023-07-14 05:35:00 -0700

    task 0

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,13 +12,15 @@ int main(void)
 	char alpha = 'a';
 	int i = 0;
 
-	while ( i <= 9 )
+	while (i <= 9)
 	{
 		putchar('0' + i);
+		i++;
 	}
-	while ( alpha <= 'f' )
+	while (alpha <= 'f')
 	{
 		putchar(alpha);
+		alpha++;
 	}
 	putchar('\n');
 	return (0);
