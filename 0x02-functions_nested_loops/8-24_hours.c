38ee99c351c5a13cd56fc68b889369b3987b6010
robbairbadeia <robber.badeia@gmail.com>
2023-07-18 11:33:03 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,44 +1,38 @@
 #include "main.h"
-
-
+/**
+ * jack_bauer - Print every minute of the day of Jack Bauer
+ * Description: Start from 00:00 to 23:59
+ */
 void jack_bauer(void)
 {
-	int i, j;
+	int x = 0;
+	int a = 0, b = 0, c = 0, d = 0;
 
-	for (i = 0; i <= 23; i++)
+	while (x < 1440)
 	{
-		for (j = 0; j < 60; j++)
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(':');
+		_putchar(c + '0');
+		_putchar(d + '0');
+		_putchar('\n');
+
+		d++;
+		if (d > 9)
 		{
-			if (i < 10)
-			{
-				if (j < 10)
-				{
-					_putchar('0');
-					_putchar('0' + i);
-					_putchar(':');
-					_putchar('0');
-					_putchar('0' + j);
-					_putchar('\n');
-				}
-			}
-			else
-			{
-				if (j < 10)
-				{
-					_putchar(i + '0');
-					_putchar(':');
-					_putchar('0');
-					_putchar(j + '0');
-					_putchar('\n');
-				}
-				else
-				{
-					_putchar(i + '0');
-					_putchar(':');
-					_putchar(j + '0');
-					_putchar('\n');
-				}
-			}
+			d = 0;
+			c++;
 		}
+		if (c > 5)
+		{
+			c = 0;
+			b++;
+		}
+		if (b > 9)
+		{
+			b = 0;
+			a++;
+		}
+		x++;
 	}
 }
