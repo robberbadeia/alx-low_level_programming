3a667eed9211a2b2ce3555c9a579a51ad02bdf3c
robbairbadeia <robber.badeia@gmail.com>
2023-07-18 03:55:24 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0.
  */
-int print_alphabet(void)
+void print_alphabet(void)
 {
 	char c;
 	
@@ -13,5 +13,4 @@ int print_alphabet(void)
 		_putchar(c);
 	}
 	_putchar('\n');	
-	return (0);
 }
