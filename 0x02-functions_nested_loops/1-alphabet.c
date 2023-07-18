b2f6c8d37ca09e86250dc8bc43ebc7aaf5602127
robbairbadeia <robber.badeia@gmail.com>
2023-07-18 03:40:22 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int c;
+	
+	for (c >= "a"; c <= "z"; c++)
+	{
+		_putchar(c);
+	}
+	
+	return (0);
+}
