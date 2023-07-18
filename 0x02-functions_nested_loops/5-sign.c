a849b662b7f58f0bfc0054e380b773ea6e142211
robbairbadeia <robber.badeia@gmail.com>
2023-07-18 05:54:12 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,16 +6,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
-		_putchar('\+');
+	//	_putchar('+');
 	}
 	else if (n < 0)
 	{
 		return (-1);
-		_putchar('-');
+	//	_putchar('-');
 	}
 	else
 	{
 		return (0);
-		_putchar('0');
+	//	_putchar('0');
 	}
 }
