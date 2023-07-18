2f1a47bec6ca785378836568408c40260378f8b1
robbairbadeia <robber.badeia@gmail.com>
2023-07-18 11:10:59 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/*
+ * print_to_98 - Function to start print numbers from @n to 98
+ * @n: int type number
+ */
+void print_to_98(int n)
+{
+	while(n < = 98)
+	{
+		if (n > 9)
+		{
+			_putchar(' ');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			_putchar(',');
+		}
+		else if (n == 98)
+		{
+			_putchar(' ');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+		else
+		{
+			_putchar(n);
+			_putchar(',');
+		}
+		n++;
+	}
+}
