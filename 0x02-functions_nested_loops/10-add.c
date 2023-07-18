c5a17bbe39f090d484d1dc119b57a5d5f265ddd7
robbairbadeia <robber.badeia@gmail.com>
2023-07-18 10:36:52 -0700

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -7,11 +7,11 @@ int add(int i, int j)
 	z = i + j;
 	if (z > 9)
 	{
-		_putchar(z / 10 + '0');
-		_putchar(z % 10 + '0');
+		return (z);
+		return (z);
 	}
 	else
 	{
-		_putchar(z + '0');
+		return (z);
 	}
 }
