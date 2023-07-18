333afae5efca59aeded743d3023e2c8331f62beb
robbairbadeia <robber.badeia@gmail.com>
2023-07-18 05:09:19 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*
+ * _islower : function to check wethear character lower case or not
+ * Return 1 if lowercase else 
+ * Return 0
+ */
+int _islower(int c)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+		{
+			return ('1');
+		}
+		else
+		{
+			return ('0');
+		}
+	}
+}
