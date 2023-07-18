e9d66bae5a47683fcaa179a800887ffd5a0e0965
robbairbadeia <robber.badeia@gmail.com>
2023-07-18 03:14:12 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,11 @@
  */
 void print_putchar(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char putchar[] = "_putchar";
 
+	for (int i = 0; i <= 7; i++)
+	{
+		_putchar(putchar[i]);
+	}
 	return (0);
 }
