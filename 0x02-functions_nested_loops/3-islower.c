ae85842d90f26159d34f2eac6de47418644ed8df
robbairbadeia <robber.badeia@gmail.com>
2023-07-18 05:27:44 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,17 +1,13 @@
 #include "main.h"
-/*
- * _islower : function to check wethear character lower case or not
- * Return 1 if lowercase else 
- * Return 0
+/**
+ * _islower - Return 1 if letter is lowercase, 0 if not.
+ * @c: char type letter
+ * Return: 1 if lowercase, 0 if not lowercase
  */
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
+	if (c > 96 && c < 123)
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
