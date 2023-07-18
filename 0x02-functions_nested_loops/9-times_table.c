2dde8e96119e0f81221a5e0e37c0b91a24c2f169
robbairbadeia <robber.badeia@gmail.com>
2023-07-18 10:04:16 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+void times_table(void)
+{
+	int i, j, z;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			z = i * j;
+
+			if (i == 9)
+			{
+				if (z > 9)
+				{
+					_putchar('0' + z);
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('0' + z);
+				}
+			}
+			else
+			{
+				if (z > 9)
+				{
+					_putchar('0' + z);
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('0' + z);
+					_putchar(',');
+					_putchar('  ');
+				}
+			}
+
+		}
+		_putchar('\n');
+	}
+
+
+}
