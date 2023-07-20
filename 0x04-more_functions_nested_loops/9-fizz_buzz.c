ba4d234c76caa6a7dbee6eb9f85c9eeca39549c2
robbairbadeia <robber.badeia@gmail.com>
2023-07-20 08:58:48 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,28 +10,28 @@
 void fizz_buzz(void)
 {
         int i;
-        for (i = 1; i <= 100; i++)
-        {
-                if (((i % 3) == 0) && ((i % 5) == 0))
-                {
-                        printf("%s ", "FizzBuzz");
-                }
-                else if ((i % 3) == 0)
-                {
-                        printf("%s ", "Fizz");
-                }
-                else if ((i % 5) == 0)
-                { 
-                    if (i == 100)
-                        {printf("%s", "Buzz");}
-                    else
-                        {printf("%s ", "Buzz");}
-                }
-                else
-                {
-                    printf("%d ", i);
-                }
-        }
-        printf("\n");
+        
+	for (i = 1; i <= 100; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else if ((i % 3) == 0 && (i % 5) != 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if ((i % 5) == 0 && (i % 3) != 0)
+		{ 
+			if (i == 100)
+				printf("%s", "Buzz");
+			else
+				printf("%s ", "Buzz");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
 }
-
