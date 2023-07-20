e3b2ad61f4b9c3bfc67bcb40af5becff1abd2562
robbairbadeia <robber.badeia@gmail.com>
2023-07-20 06:14:35 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * print_line - Function to print @n of '_'
- *@n : integer number 
- */
+ *@n: integer number
+*/
 void print_line(int n)
 {
-	if(n > 0)
+	if (n > 0)
 	{
 		int i;
 
-		for(i = 0; i < n; i++)	
+		for (i = 0; i < n; i++)
 		{
-			_putchar('_');
+			_putchar ('_');
 		}
 	}
 	_putchar('\n');
