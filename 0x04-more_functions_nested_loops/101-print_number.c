a3c9a1308b13ed1421d4cbfa41d843e99fdfef70
robbairbadeia <robber.badeia@gmail.com>
2023-07-20 12:59:21 -0700

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_number - Function to print number entered by user
+ * @n: int number variable
+*/
+void print_number(int n)
+{
+	if(n >= 0)
+	{
+		if(n <= 9)
+		{
+			_putchar('n');
+		}
+		else
+		{
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+	}
+	else
+	{
+		if (n >= -9)
+		{
+			_putchar('-');
+			_putchar(n);
+		}
+		else
+		{
+			_putchar('-');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
+	}
+
+}
