3b314b66ebeb079188170dbaa586fefea5382a92
robbairbadeia <robber.badeia@gmail.com>
2023-07-20 03:37:21 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
-
 /**
- *print_numbers - Function to print numbers from 0 to 9 followed by new line
- *
- **/
+*print_numbers- a function that prints the numbers, from 0 to 9
+*
+*Return: print to stdout the alphabet, in lowercase, followed by a new line.
+**/
 void print_numbers(void)
 {
 	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i);
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
