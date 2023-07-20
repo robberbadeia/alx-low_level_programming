3adb16d08e312358fcba878292536b883d361fe4
robbairbadeia <robber.badeia@gmail.com>
2023-07-20 04:15:03 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - Function to print int numbers from 0 to 14 ten times
+ **/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+}
