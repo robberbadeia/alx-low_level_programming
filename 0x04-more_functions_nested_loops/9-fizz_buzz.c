ffbaf75a8d491af74e6776021dcce60d15cb143f
robbairbadeia <robber.badeia@gmail.com>
2023-07-20 08:47:28 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * fizz-buzz - Fuction to prints
+ * fizz: if the number divided by 3
+ * buzz if the number divided by 5
+ * fizzbuzz if the number divided by 3 & 5
+*/
+
+void fizz_buzz(void)
+{
+        int i;
+        for (i = 1; i <= 100; i++)
+        {
+                if (((i % 3) == 0) && ((i % 5) == 0))
+                {
+                        printf("%s ", "FizzBuzz");
+                }
+                else if ((i % 3) == 0)
+                {
+                        printf("%s ", "Fizz");
+                }
+                else if ((i % 5) == 0)
+                { 
+                    if (i == 100)
+                        {printf("%s", "Buzz");}
+                    else
+                        {printf("%s ", "Buzz");}
+                }
+                else
+                {
+                    printf("%d ", i);
+                }
+        }
+        printf("\n");
+}
+
