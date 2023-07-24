a1a87b5113084881cef14b8da5ac4e203738baad
robbairbadeia <robber.badeia@gmail.com>
2023-07-24 01:14:13 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_strlen - Function returns the lenght of string
+ *@s: user input string
+ *Return: int value
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	while(s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
