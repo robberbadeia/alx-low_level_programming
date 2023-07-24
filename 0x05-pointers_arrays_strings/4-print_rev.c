29cd5c310068a075e04116f473a0eeb8f891e02f
robbairbadeia <robber.badeia@gmail.com>
2023-07-24 03:18:04 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,14 @@ void print_rev(char *s)
 	int i;
 
 	i = 0;
-	while(s[i] != '\0')
+	while (s[i])
 	{
 		i++;
 	}
-	while(i > 0)
+	while (i >= 0)
 	{
 		_putchar(s[i]);
 		i--;
 	}
-	_putchar('\n');
+	_putchar ('\n');
 }
