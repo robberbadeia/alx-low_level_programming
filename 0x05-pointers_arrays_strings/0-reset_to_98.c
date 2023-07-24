492ba3aea1f47b0dd87c8fba0472e414978d8eec
robbairbadeia <robber.badeia@gmail.com>
2023-07-24 00:36:50 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * reset_to_98 - Function to rese pointer to 98
- * @*n: input to update to 98.
-*/
+ * @n: input to update to 98.
+**/
 void reset_to_98(int *n)
 {
 	*n = 98;
