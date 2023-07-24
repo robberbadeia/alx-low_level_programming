58383571d2416928d52590b2b215aa248e17f068
robbairbadeia <robber.badeia@gmail.com>
2023-07-24 06:23:26 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,14 +15,18 @@ void puts_half(char *str)
 	if (i % 2 == 0)
 	{
 		n = i / 2;
+		for (j = n; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
 	}
 	else
 	{
 		n = (i-1) / 2;
-	}
-	for (j = n; j < i; j++)
-	{
-		_putchar(str[j]);
+		for (j = n; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
 	}
 	_putchar('\n');
 }
