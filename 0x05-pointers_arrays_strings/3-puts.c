34535288ab412c6aeede10bff03f321a4da01d72
robbairbadeia <robber.badeia@gmail.com>
2023-07-24 02:05:33 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ *_puts - Function to print string
+ *@str: user input string
+ *
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
