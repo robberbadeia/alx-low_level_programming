8637a9814335bc0fdd029191c6df071d7ea62fba
robbairbadeia <robber.badeia@gmail.com>
2023-07-24 06:57:36 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_strcpy - Function to copy string from source to detination
+ *@src: user input string
+ *@dest: destination will string copied to
+ *Return: the value of @dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while(src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
