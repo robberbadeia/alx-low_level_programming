33772cc0657383eb079dc86c6dc0577b567239de
robbairbadeia <robber.badeia@gmail.com>
2023-07-24 09:34:51 -0700

    header upd

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,6 +7,8 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 int _putchar(char c);
 
