c8e64bc0644b336451ec64d4d188199e2f875cdf
robbairbadeia <robber.badeia@gmail.com>
2023-07-24 06:36:21 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_array - Function to prin n array elements
+ *@n: user input number of elements to be printed
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i = n)
+			printf("%d", a[i]);
+		else
+			printf("%d, ", a[i]);
+	}
+}
