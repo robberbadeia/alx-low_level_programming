19da7084b16e2a5e189419db577cb0bd7b517940
robbairbadeia <robber.badeia@gmail.com>
2023-07-26 14:47:46 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *string_toupper - Function to change all lower case letters to upper
+ *@a: user input array pointer
+ *Return: array pointer
+ */
+char *string_toupper(char *a)
+{
+	int i;
+
+	while(s[i] != '\0')
+	{
+		if (a[i] >= 97 && a[i] <= 122)
+		{
+			a[i] = a[i] - 32;
+		}
+		i++;
+	}
+	return (a);
+}
