f5be79ba57ada7f45827915c6de182445fd25ac2
robbairbadeia <robber.badeia@gmail.com>
2023-07-26 09:30:08 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,11 +12,11 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	i = j = c = 0;
-	while (dest[i] != '\0')
+	while (dest[i])
 	{
 		i++;
 	}
-	while (src[j] != '\0')
+	while (src[j])
 	{
 		j++;
 	}
