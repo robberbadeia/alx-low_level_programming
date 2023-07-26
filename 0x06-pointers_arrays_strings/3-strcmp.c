7def7f3684c6e573d578f0e3b907d61037750565
robbairbadeia <robber.badeia@gmail.com>
2023-07-26 04:23:47 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strcmp - Function to compare to arrays
+ *@s1: user input array pointer
+ *@s2: user array pointer
+ *Return: integer number
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, n;
+
+	i = n = 0;
+	for (i =0; s1[i] != '\0'; i++)
+	{
+		n = n + s1[i] - s2[i];
+	}
+	return (n);
+}
