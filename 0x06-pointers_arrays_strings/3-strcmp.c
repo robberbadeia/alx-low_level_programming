bb773fd0220639af864639392b3a1d7202908245
robbairbadeia <robber.badeia@gmail.com>
2023-07-26 12:57:05 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,14 @@ int _strcmp(char *s1, char *s2)
 	i = n = 0;
 	for (i = 0; ; i++)
 	{
-		n = n + s1[i] - s2[i];
+		if ((s1[i] != '\0') && (s2[i] != '\0'))
+			n = n + s1[i] - s2[i];
+
+		else if ((s1[i] == '\0') && (s2[i] != '\0'))
+			n = n - s2[i];
+
+		else if ((s1[i] != '\0') && (s2[i] == '\0'))
+			n = n + s1[i];
 	}
 	return (n);
 }
