6186865025455cdfc68e572f8c70b23f2030cd39
robbairbadeia <robber.badeia@gmail.com>
2023-07-26 05:59:19 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *reverse_array - Function to reverse the array contents
+ *@a: user array pointer input
+ *@n: number of array elements
+ */
+void reverse_array(int *a, int n)
+{
+	in i;
+
+	n--;
+	for (i = 0; i < n; i++)
+	{
+		a[i] = a[n - i];
+	}
+}
