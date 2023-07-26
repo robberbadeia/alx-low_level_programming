b4e2faae3360d25ab87b86a39618f228cb3c8a7c
robbairbadeia <robber.badeia@gmail.com>
2023-07-26 11:29:28 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	i = 0;
-
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
