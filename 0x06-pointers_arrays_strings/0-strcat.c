c456e826b94e5d2f0a8230ee77953866083c271f
robbairbadeia <robber.badeia@gmail.com>
2023-07-26 02:21:24 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *_strcat - Function to append src char array to dest array
+ *@src: user char array input
+ *@dest: user char array input
+ *Return: char array pointer
+ *
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j, n;
+
+	i = j = n =0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		j++;
+	}
+
+	for (n = i; n <= j; n++)
+	{
+		dest[n] = src[n - i];
+	}
+	return (dest);
+}
