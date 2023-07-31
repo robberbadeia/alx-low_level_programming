86f1e6a6ea02ba20e32aa152618a2eebdebe3343
robbairbadeia <robber.badeia@gmail.com>
2023-07-31 10:05:58 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *_strpbrk - Function to find first match
+ *@s: input
+ *@accept: input
+ *Return: array
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, *add;
+
+	i = add = 0;
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				add = &s[i];
+				break;
+			}
+		}
+		if (add != 0)
+			break;
+		i++;
+	}
+	while (s[i])
+	{
+		return (s[i]);
+	}
+}
