d156c74465eac150c6b5d1c06f264267a8657d77
robbairbadeia <robber.badeia@gmail.com>
2023-07-31 03:10:10 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_memset - Function to fill a memory with specific data
+ *@s: user input array
+ *@b: user input data
+ *@n: user integer input 
+ *Return: array address
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i ;
+
+	for (i = 0; i < n; i++)
+	{
+		*s = b;
+	}
+	return (s);
+}
