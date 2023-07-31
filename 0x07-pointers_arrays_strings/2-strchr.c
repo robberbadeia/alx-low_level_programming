a11e453c7489ec72c19b784bcebcf3675d109aed
robbairbadeia <robber.badeia@gmail.com>
2023-07-31 03:49:45 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,19 +1,16 @@
 #include "main.h"
 /**
- *
- *
- *
- *
- *
- *
- *
+ *_strchr - Function to find a character in array
+ *@s: input array
+ *@c: input char by user
+ *Return: array
  */
 char *_strchr(char *s, char c)
 {
 	int i;
 
 	i = 0;
-	while(s[i] '\0')
+	while (s[i] '\0')
 	{
 		if (s[i] == 'c')
 		{
