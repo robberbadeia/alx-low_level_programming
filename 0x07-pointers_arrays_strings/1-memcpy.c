4f65db4cd4c8bf3398fba9b1928bd9f136ddd2fd
robbairbadeia <robber.badeia@gmail.com>
2023-07-31 03:37:39 -0700

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_memcpy - Function to copy array content to another array
+ *@src: user input array
+ *@dest: user destination array
+ *@n: array size
+ *Return: dest array
+ *
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
