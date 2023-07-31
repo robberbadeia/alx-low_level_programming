e480495090da25c9c8cc1c6fecbaf3fce82bfe67
robbairbadeia <robber.badeia@gmail.com>
2023-07-31 04:43:08 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,7 +12,13 @@ char *_strchr(char *s, char c)
 	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
+	{
 		if (s[i] == c)
+		{
+			return (&s[i]);
 			break;
-	return (s[i] == c ? &s[i] : '\0');
+		}
+	}
+	if (s[i] != c)
+		return ('\0');
 }
