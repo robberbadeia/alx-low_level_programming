6e408c87ad39d2612525f3e629102b13b4c37728
robbairbadeia <robber.badeia@gmail.com>
2023-07-31 03:17:58 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,12 +3,12 @@
  *_memset - Function to fill a memory with specific data
  *@s: user input array
  *@b: user input data
- *@n: user integer input 
+ *@n: user integer input
  *Return: array address
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i ;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
