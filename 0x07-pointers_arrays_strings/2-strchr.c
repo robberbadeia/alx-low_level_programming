dc174e047cee34aa56844f41e267540b77b3b4fa
robbairbadeia <robber.badeia@gmail.com>
2023-07-31 03:46:20 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while(s[i] '\0')
+	{
+		if (s[i] == 'c')
+		{
+			return (s[i]);
+		}
+		i++;
+	}
+
+}
