3167e6b45715996d6ae81975319790efcfe0b8a0
robbairbadeia <robber.badeia@gmail.com>
2023-07-31 11:18:52 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *_strstr - Function
+ *@haystack: input
+ *@needle: input
+ *Return: array
+ *
+ *
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	i = j = 0;
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[j])
+		{
+			while (haystack[i + j] == needle[j] && needle[j] != '\0')
+			{
+				j++;
+			}
+			if (haystack[i + j] != needle[j] || needle[j] != '\0')
+			{
+				return (haystack + i);
+			}
+		}
+		i++;
+	}
+	return ('\0');
+}
