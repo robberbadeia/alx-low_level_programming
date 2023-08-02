301ab83c45d841848301536b4032ace26603de31
robbairbadeia <robber.badeia@gmail.com>
2023-08-02 02:06:27 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - Function to print array 
+ *@s: array
+ */
+void _print_rev_recursion(char *s)
+{
+	_putchar(*S);
+	s++;
+	_print_rev_recursion(s);
+}
