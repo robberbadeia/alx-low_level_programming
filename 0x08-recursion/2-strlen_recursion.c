32b56cdc520797df7002a875631aa76a0f8ae083
robbairbadeia <robber.badeia@gmail.com>
2023-08-02 05:07:24 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_strlen_recursion - Function to count string lenght
+ *@s: input array
+ *Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s);
+}
