2f794ddda0b20387e83dbe88d7161a945b9bfc0a
robbairbadeia <robber.badeia@gmail.com>
2023-08-02 09:03:30 -0700

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *factorial - Function to print faactorial
+ *@n: int input
+ *Return: int
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
