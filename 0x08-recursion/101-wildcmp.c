7406d7045bd1980f7170fc8df72d42f2b6211180
robbairbadeia <robber.badeia@gmail.com>
2023-08-03 03:51:27 -0700

    Task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *wildcmp - Function to check string
+ *@s1: input
+ *@s2: input
+ *Return: int
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 != *s2)
+		return (o);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s1 != *s2 && *s2 == '*')
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (1);
+}
