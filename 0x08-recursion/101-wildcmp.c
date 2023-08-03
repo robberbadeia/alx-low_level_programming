93869fe3e46e9b4a2a664c317def746ef2a8baa7
robbairbadeia <robber.badeia@gmail.com>
2023-08-03 03:57:27 -0700

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,9 +7,9 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 != *s2)
+	if (*s1 != *s2 && *s2 != '*')
 		return (0);
-	if (*s1 == *s2)
+	if (*s1 == *s2 && *s1 != '\0')
 		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s1 != *s2 && *s2 == '*')
 		return (wildcmp(s1 + 1, s2 + 1));
