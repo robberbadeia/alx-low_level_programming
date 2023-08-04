47d93a4a0642250b1a9900e5e49ce12fd8a433ea
robbairbadeia <robber.badeia@gmail.com>
2023-08-04 05:56:34 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *main - Function to print app name
+ *@argc: size
+ *@argv: array
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++
+			printf("%s", argv[i]);
+	return (0);
+}
