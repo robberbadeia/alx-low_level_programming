03260be65beed9240be8f1aab28415d0f64be5ae
robbairbadeia <robber.badeia@gmail.com>
2023-08-07 12:03:38 -0700

    Task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,13 +10,10 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (height <= 0)
-		return (NULL);
-
-	if (grid == NULL)
-		return (NULL);
-
-	for (i = 0; i < height; i++)
-		free(grid[i]);
-	free(grid);
+	if (height != 0 && grid != NULL)
+	{
+		for (i = 0; i < height; i++)
+			free(grid[i]);
+		free(grid);
+	}
 }
