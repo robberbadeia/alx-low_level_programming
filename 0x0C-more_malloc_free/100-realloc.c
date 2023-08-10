9b536b362e0bbef72f37a2eb0c87b1092fc8e3c7
robbairbadeia <robber.badeia@gmail.com>
2023-08-10 13:03:26 -0700

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,14 +10,14 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *p;
+	char *p;
 	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
-	if (ptr == NULL)
+	if (ptr == 0)
 		return (malloc(new_size));
-	if (new_size == 0 && ptr)
+	if (new_size == 0 && ptr != 0)
 	{
 		free(ptr);
 		return (NULL);
