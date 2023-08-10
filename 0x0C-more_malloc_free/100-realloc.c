6219d9d7f3673e76dd715f4b8651d12f700c4767
robbairbadeia <robber.badeia@gmail.com>
2023-08-10 12:41:43 -0700

    Task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,36 @@
+#include "main.hh"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_realloc - Function
+ *@ptr: input
+ *@old_size: input
+ *@new_size: input
+ *Return: pointer to new mem
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *p;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	p = malloc(new_size);
+
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	for (i = 0; i < old_size; i++)
+		p[i] = ptr[i];
+	return (p);
+}
