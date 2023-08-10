cd8ff868e5ef11271c47f1a9fe5400ed880931da
robbairbadeia <robber.badeia@gmail.com>
2023-08-10 13:27:51 -0700

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,13 +1,15 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+
 /**
- *_realloc - Function
- *@ptr: input
- *@old_size: input
- *@new_size: input
- *Return: pointer to new mem
- */
+* _realloc - a function that reallocates a memory block using malloc and free
+* @ptr: is a pointer to the memory previously allocated
+* @old_size: is the size, in bytes, of the allocated space for ptr
+* @new_size: is the new size, in bytes of the new memory block
+* Return: a pointer
+**/
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *p;
@@ -16,25 +18,22 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	i = 0;
 	if (new_size == old_size)
 		return (ptr);
-
 	if (ptr == NULL)
 		return (malloc(new_size));
-
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	p = malloc(new_size);
-
 	if (p == NULL)
 		return (NULL);
-
+	if (new_size < old_size)
+		old_size = new_size;
 	while (i < old_size)
 	{
-		(char *)p[i] = (char *)ptr[i];
-		i++;
+		*((char *)p + i) = *((char *)ptr + i);
+		i += 1;
 	}
 	free(ptr);
 	return (p);
