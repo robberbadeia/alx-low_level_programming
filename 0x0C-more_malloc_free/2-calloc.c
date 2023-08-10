698ae94ca30f34e97e6675b4e06f65b6e5040917
robbairbadeia <robber.badeia@gmail.com>
2023-08-10 10:20:55 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,14 +11,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *p;
+	char *p;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(nmemb * size * sizeof(int));
+	p = malloc(nmemb * size);
 
 	if (p == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		p[i] = 0;
 
 	return (p);
 }
