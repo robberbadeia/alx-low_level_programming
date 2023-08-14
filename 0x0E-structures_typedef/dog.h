b82b77c0523d38c32e1a624c0f0b9a575faf2baa
robbairbadeia <robber.badeia@gmail.com>
2023-08-14 02:56:18 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - creat a dog struct
+ *@name: pointer
+ *@age: float number
+ *@owner: pointer
+ *Description: dog struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
