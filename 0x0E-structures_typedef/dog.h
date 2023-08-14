c56c8c95e1c43ee15c142d293c4738e970bd2e72
robbairbadeia <robber.badeia@gmail.com>
2023-08-14 10:45:32 -0700

    hdr upd

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,13 +14,15 @@ struct dog
 	char *owner;
 };
 
+/**
+ *dog_t - Typedef for dog
+ *
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
-/**
- *dog_t - Typedef for dog struct
- *
- */
-typedef struct dog dog_t;
+
 #endif
