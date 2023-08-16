63066bd855267a28fe6d6fc1a30cfe28af4eec36
robbairbadeia <robber.badeia@gmail.com>
2023-08-16 10:47:45 -0700

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,36 +1,45 @@
-#include "3-calc.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include "3-calc.h"
 /**
- *main - Function
- *@argc: input
- *@argv: input
- *Return: int
- */
+ * main - main
+ * @argc: arg count
+ * @argv: arg array
+ *
+ * Return: int
+ *
+ **/
 int main(int argc, char **argv)
 {
-	int i, j;
+	int inta, intb;
 	int (*f)(int, int);
 
-	i = j = 0;
 	if (argc != 4)
 	{
 		printf("Error\n");
 		return (98);
 	}
-	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' &&
-			 argv[2][0] != '/' && argv[2][0] != '%') || strlen(argv[2]) != 1)
+
+	f = get_op_func(argv[2]);
+
+	if (f == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '/' &&
+	      argv[2][0] != '*' && argv[2][0] != '%') || strlen(argv[2]) != 1)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	i = atoi(argv[1]);
-	j = atoi(argv[3]);
 
-	f = get_op_func(argv[2]);
+		inta = atoi(argv[1]);
+		intb = atoi(argv[3]);
 
-	printf("%d\n", f(i, j));
+		printf("%d\n", f(inta, intb));
 
-	return (0);
-}
+		return (0);
+
