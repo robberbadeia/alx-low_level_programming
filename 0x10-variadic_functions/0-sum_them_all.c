17e7b9a8a6e5ea72c693eb7883790f08253c52fe
robbairbadeia <robber.badeia@gmail.com>
2023-08-17 04:19:13 -0700

    Task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ *
+ *sum_them_all - Function to return int
+ *@n: int
+ *
+ *Return: int
+ *
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum;
+	va_list ap;
+
+	sum = 0;
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	       sum += va_arg(ap, int);
+	va_end(ap);
+	return (sum);
+}
