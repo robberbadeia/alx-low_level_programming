fe527d9d6280a76f001ef6a8cacc1cb393d83c8f
robberbadeia <robber.badeia@gmail.com>
2023-08-24 07:04:49 -0700

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *free_list - Function
+ *@head: input
+ *
+ *Return: Non
+ */
+void free_list(list_t *head)
+{
+	list_t *curr;
+
+	while (head)
+	{
+		curr = *head;
+		head = head->next;
+		free{curr);
+	}
+}
