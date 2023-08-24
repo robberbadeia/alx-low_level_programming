0cafd02efc5e2739e08c58ec1bd274e7a4ef6f59
robberbadeia <robber.badeia@gmail.com>
2023-08-24 04:40:50 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *print_list -Function
+ *@h: input
+ *
+ *
+ *Return: output
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	while (!h)
+	{
+		i++;
+	}
+	return (i);
+}
