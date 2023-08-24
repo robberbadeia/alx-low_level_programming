e6fd55d70d11bc319239fefeb228221d0d8f257d
robberbadeia <robber.badeia@gmail.com>
2023-08-24 04:54:20 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,9 +15,9 @@ size_t print_list(const list_t *h)
 		if (h->str)
 			printf("[%d] %s\n", h->len, h->str);
 		else
-			printf("[0] (nil)");
-		i++;
+			printf("[0] (nil)\n");
 		h = h->next;
+		i++;
 	}
 	return (i);
 }
