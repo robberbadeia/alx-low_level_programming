d552a59f68d2e25489f139043dcae1f4182a45bc
robberbadeia <robber.badeia@gmail.com>
2023-08-24 05:09:51 -0700

    Task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *add_node - Function
+ *@head: input
+ *@str: input
+ *Return: list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t nlst;
+	int i = 0;
+
+	if (!head)
+		return (NULL);
+	
+	while (str != '\0')
+		i++;
+
+	lst->len = i;
+	lst->str = strdup(h->str);
+	lst->next = h->next;
+
+	return (lst);
+}
