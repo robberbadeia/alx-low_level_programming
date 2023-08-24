72a167998fe7f8807c1a347be659325bdff2e40d
robberbadeia <robber.badeia@gmail.com>
2023-08-24 07:14:50 -0700

    Task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,9 +10,11 @@ void free_list(list_t *head)
 {
 	list_t *current;
 
-	while ((current = head) != NULL)
+	while (head != NULL)
 	{
+		current = *head;
 		head = head->next;
+		free(current->str);
 		free(current);
 	}
 }
