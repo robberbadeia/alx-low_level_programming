3e50745d405e8342d7d3fb4a9ba680cbaef60ee8
robberbadeia <robber.badeia@gmail.com>
2023-08-28 09:11:37 -0700

    HDR

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -13,8 +13,8 @@
 */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 
@@ -29,5 +29,4 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
-#endif // LISTS_H_INCLUDED
-
+#endif
