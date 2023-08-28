d7455d6a1c86011b990b4824b395784d04a7170b
robberbadeia <robber.badeia@gmail.com>
2023-08-28 13:36:28 -0700

    Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,20 +1,22 @@
 #include "lists.h"
+
 /**
- *free_listint2 - Function
- *@head: input
- *
- *Return: Non
+ * free_listint2 - frees a linked list, and sets head to NULL
+ * @head: linked list
  */
+
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *ptr;
 
-	if (!*head)
+	if (head == NULL)
 		return;
-	while (*head)
+
+	while (*head != NULL)
 	{
-		tmp = *head;
-		*head = tmp->next;
-		free(tmp);
+		ptr = *head;
+		*head = ptr->next;
+		free(ptr);
 	}
+
 }
