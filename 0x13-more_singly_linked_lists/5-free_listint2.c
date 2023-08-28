f867087b440fdeb3b7088f14009744bf66761088
robberbadeia <robber.badeia@gmail.com>
2023-08-28 13:21:53 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *free_listint2 - Function
+ *@head: input
+ *
+ *Return: Non
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t tmp;
+
+	if (!*head)
+		return;
+	while (*head)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
