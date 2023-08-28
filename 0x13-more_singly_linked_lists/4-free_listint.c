da25f4d12bd8a069c1d5a7e0d091aff0adde2ebf
robberbadeia <robber.badeia@gmail.com>
2023-08-28 12:37:31 -0700

    Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *free_listint - Function
+ *@head: input
+ *
+ *Return: Non
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	if (!head)
+		return;
+
+	while (!head)
+	{
+		tmp = head;
+		free(tmp);
+		head = head->next
+	}
+}
