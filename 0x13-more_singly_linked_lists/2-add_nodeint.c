b9a0f5f7323ab30f5c816e6d4a5dcdad9d4184be
robberbadeia <robber.badeia@gmail.com>
2023-08-28 09:41:45 -0700

    Task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ *add_nodeint - Function
+ *
+ *@head: pointer
+ *@n: int
+ *Return: list
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t nNode;
+
+	if (!head)
+		return (NULL);
+	nNode = malloc(sizeof(listint_t));
+	if (!nNode)
+		return (NULL);
+
+	nNode->n = n;
+	nNode->next = (*head)->next;
+	*head = nNode;
+	return (nNode);
+}
