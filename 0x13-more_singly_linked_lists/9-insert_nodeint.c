592027bd10dacda7a23fd6556099de92a3d27f9c
robberbadeia <robber.badeia@gmail.com>
2023-08-29 05:20:30 -0700

    Task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ *insert_nodeint_at_index - Function
+ *
+ *@head: input
+ *@idx: input
+ *@n: input
+ *Return: pointer
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *ptr, *tmp, *new;
+
+	if (!*head)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	ptr = *head;
+	while (ptr && (i < (idx - 1)))
+	{
+		ptr = ptr->next;
+		i++;
+	}
+	new->n = n;
+	new->next = ptr->next;
+	ptr->next = new;
+	return(new);
+}
