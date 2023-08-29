1560773be4a163e257a0879074a02bef859a7801
robberbadeia <robber.badeia@gmail.com>
2023-08-29 08:59:20 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	ptr = *head;
+	if (idx == 0)
+	{
+		new->next = ptr;
+		ptr->next = new;
+		return (new);
+	}
 	while (ptr && (i < (idx - 1)))
 	{
 		ptr = ptr->next;
