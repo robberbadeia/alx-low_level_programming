ea91ff7d78ec68beecf6026013b0353f69e1bb94
robberbadeia <robber.badeia.com>
2023-10-12 15:46:53 +0300

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,28 +8,32 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	size_t len = 0;
-	unsigned int i = 0;
+	dlistint_t *new_mem, *res_mem = *h;
+	/* unsigned int counter = 0 */
 
-	while (*h)
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
 	{
-		len++;
-		(*h) = (*h)->next;
+		if (res_mem == NULL)
+			return (NULL);
+		res_mem = res_mem->next;
 	}
-	len--;
-	if (*h == NULL)
-	{
-		return(readd_dnodeint(*h, n));
-	}
-	if (idx == len)
-	{
-		return(add_dnodeint_end(*h, n));
-	}
-	while (i < idx)
-	{
-		(*h) = (*h)->next;
-		i++;
-	}
-	return (*h);
-	
+
+	if (res_mem->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new_mem = malloc(sizeof(dlistint_t));
+
+	if (new_mem == NULL)
+		return (NULL);
+
+	new_mem->n = n;
+	new_mem->next = res_mem->next;
+	new_mem->prev = res_mem;
+	res_mem->next->prev = new_mem;
+	res_mem->next = new_mem;
+
+	return (new_mem);
 }
