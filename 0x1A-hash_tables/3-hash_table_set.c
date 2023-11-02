4b9f7abefa7ac5eb55fdf3aa558204eb8f075606
robberbadeia <robber.badeia@gmail.com>
2023-11-02 23:02:24 +0200

    T03

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,9 +8,11 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int idx = 0, i = 0;
+	unsigned long int idx = 0;
+	unsigned long int i = 0;
 	hash_node_t *new;
 	char *val;
+
 	/*Input Checks*/
 	if (!key || !ht || !value)
 	{
