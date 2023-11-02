e0aca0d7817bd0939ab18c68c7b7b60501f6a678
robberbadeia <robber.badeia@gmail.com>
2023-11-02 22:57:09 +0200

    T0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 
+	/*Input Checks*/
 	if (!size)
 		return (NULL);
 
 	table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
 
 	table->size = size;
 
 	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
+	if (!table->array)
+		return (NULL);
+
 	return (table);
 }
