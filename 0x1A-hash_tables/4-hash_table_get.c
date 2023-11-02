55bcd21c1a33a82b60a6dd65e8949d77a0249ed3
robberbadeia <robber.badeia@gmail.com>
2023-11-02 11:44:13 +0200

    Task04

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - Function
+ * @ht: input
+ * @key: key
+ * Return: pointer to value
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int idx = 0, i = 0;
+
+	/*Input Checks*/
+	if (!ht || !key)
+	{
+		return (NULL);
+	}
+
+	/*Creat Index for this key*/
+	idx = key_index((const unsigned char *)key, ht->size);
+
+	/*Search for the key start from this index*/
+	for (i = idx; ht->array[i]; i++)
+	{
+		if (strcmp(key, ht->array[i]->key) == 0)
+		{
+			return (ht->array[i]->value);
+		}
+	}
+	return (NULL);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -42,4 +42,6 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 /*Task03*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+/*Task04*/
+char *hash_table_get(const hash_table_t *ht, const char *key);
 #endif
