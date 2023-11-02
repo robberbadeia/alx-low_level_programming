5f2e26dd874febb836e2c94dae5356c63a7456ec
robberbadeia <robber.badeia@gmail.com>
2023-11-02 23:45:41 +0200

    T04

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -18,12 +18,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	idx = key_index((const unsigned char *)key, ht->size);
 
 	/*Search for the key start from this index*/
-	for (i = idx; ht->array[i]->next; i++)
+	for (i = idx; ht->array[i]; i++)
 	{
-		if (strcmp(key, ht->array[i]->key) == 0)
-		{
+		if (strcmp(ht->array[i]->key, key) == 0)
 			return (ht->array[i]->value);
-		}
 	}
 	return (NULL);
 }
